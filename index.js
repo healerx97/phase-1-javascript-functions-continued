718f1e06658b534adeb13ae1cac41aa97ebadad5
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-17 18:46:42 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(act = "roller-skate") {
+    return `This Saturday, I want to ${act}!`
+}
+
+function mondayWork(act = "go to the office") {
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(spec = "*") {
+    return function(adj) {
+        return `You are ${spec}${adj}${spec}!`
+    }
+}
